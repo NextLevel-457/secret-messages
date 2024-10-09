2aa338b2b23cde1c93f0b5080f70821b96b4becb
NextLevel <167141032+NextLevel-457@users.noreply.github.com>
2024-10-09 14:47:58 -0700

    Update create.js

--- create.js
+++ create.js
@@ -4,7 +4,7 @@ function getRandomInt(max) {
 
 function createAccount() {
   const username = document.getElementById("input").value + getRandomInt(99999).toString();
-  document.getElementById("link1").innerHTML = "Share this link with your friends so that they can send you secret messages: https//nextlevel-457.github.io/secret-messages/" + username;
-  document.getElementById("link2").innerHTML = "Use this link to see your secret messages: https//nextlevel-457.github.io/secret-messages/read.html?code=" + username;
+  document.getElementById("link1").innerHTML = "Share this link with your friends so that they can send you secret messages: https://nextlevel-457.github.io/secret-messages/" + username;
+  document.getElementById("link2").innerHTML = "Use this link to see your secret messages: https://nextlevel-457.github.io/secret-messages/read.html?code=" + username;
   document.getElementById("tip").innerHTML = "It's worth bookmarking this so that you can come back to it later!"
 }
