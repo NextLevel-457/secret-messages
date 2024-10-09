5704a67a2b44558bdc4f80212a9a02d9a15d3c0f
NextLevel <167141032+NextLevel-457@users.noreply.github.com>
2024-10-09 14:39:35 -0700

    Update create.js

--- create.js
+++ create.js
@@ -4,6 +4,6 @@ function getRandomInt(max) {
 
 function createAccount() {
   const username = document.getElementById("input").value + getRandomInt(99999).toString();
-  document.getElementById("link1").innerHTML = "https//nextlevel-457.github.io/secret-messages/${username}";
-  document.getElementById("link2").innerHTML = "https//nextlevel-457.github.io/secret-messages/read.html?code=${username}";
+  document.getElementById("link1").innerHTML = "https//nextlevel-457.github.io/secret-messages/" + username;
+  document.getElementById("link2").innerHTML = "https//nextlevel-457.github.io/secret-messages/read.html?code=" + username;
 }
